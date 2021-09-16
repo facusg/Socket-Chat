33f5199c7ed4fa2586de3e02cb95147c84d367e4
Facundo <facusgalante@gmail.com>
2021-09-16 14:52:16 -0300

    Mensaje a chat general en el back terminado

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -1,6 +1,6 @@
 const { io } = require("../server");
 const { Usuario } = require("../classes/usuarios");
-
+const { crearMensaje } = require("../utils/utils");
 const usuarios = new Usuario();
 
 io.on("connection", (client) => {
@@ -15,13 +15,19 @@ io.on("connection", (client) => {
     callback(personas);
   });
 
+  client.on("crearMensaje", (data) => {
+    let persona = usuarios.obtenerPersona(client.id);
+    let mensaje = crearMensaje(persona.nombre, data.mensaje);
+    client.broadcast.emit("crearMensaje", mensaje);
+  });
+
   client.on("disconnect", () => {
     let personaBorrada = usuarios.eliminarPersona(client.id);
 
-    client.broadcast.emit("crearMensaje", {
-      usuario: "Administrador",
-      mensaje: `${personaBorrada.nombre} abandono el chat`,
-    });
+    client.broadcast.emit(
+      "crearMensaje",
+      crearMensaje("Administrador", `${personaBorrada.nombre} abandono el chat`)
+    );
     client.broadcast.emit("listPersonas", usuarios.obtenerPersonas());
   });
 });
